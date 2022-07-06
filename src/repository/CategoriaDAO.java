9f7bc3bde71617369e3eb631c68b1bb600e2dd3c
David Ribeiro <103072439+DavidRBraga@users.noreply.github.com>
2022-07-06 13:33:13 -0300

    Add files via upload

--- src/repository/CategoriaDAO.java
+++ src/repository/CategoriaDAO.java
@@ -1,6 +1,68 @@
 package repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import connectionFactory.ConnectionFactory;
+import model.Categoria;
+
+
+
 public class CategoriaDAO {
+	private Connection conexao;
+
+	public CategoriaDAO() {
+		this.conexao = new ConnectionFactory().conectar();
+	}
+
+	public void insert(Categoria categoria) throws SQLException {
+		String sql = "insert into T_AUTO_CATEGORIA (cd_categoria,nm_categoria,ds_categoria) values (?,?,?)";
+		PreparedStatement stmt = conexao.prepareStatement(sql);
+
+		stmt.setLong(1, categoria.getCd_categoria());
+		stmt.setString(2, categoria.getNm_categoria());
+		stmt.setString(3, categoria.getDs_categoria());
+
+		stmt.execute();
+		stmt.close();
+
+	}
+	public List<Categoria> select() throws SQLException {
+		List<Categoria> usuarios = new ArrayList<Categoria>();
+		String sql = "select * from t_auto_Categoria";
+		PreparedStatement stmt = conexao.prepareStatement(sql);
+		ResultSet rs = stmt.executeQuery();
+		while (rs.next()) {
+			Categoria categoria = new Categoria();
+			categoria.setCd_categoria(rs.getLong("Codigo: "));
+			categoria.setNm_categoria(rs.getString("Nome do servico: "));
+			categoria.setDs_categoria(rs.getString("Descrição do serviço: "));
+
+		}
+		rs.close();
+		stmt.close();
+		return usuarios;
+	}
+	public void delete(Long Cd_categoria) throws SQLException {
+		String sql = "delete from t_auto_categoria where Cd_categoria=?";
+		PreparedStatement stmt = conexao.prepareStatement(sql);
+		stmt.setLong(1, Cd_categoria);
+		stmt.execute();
+		stmt.close();
+	}
+	public void update(Categoria categoria) throws SQLException {
+		String sql = "update t_auto_categoria set Nm_categoria=?, Ds_categoria=?,where Cd_categoria=?";
+		PreparedStatement stmt = conexao.prepareStatement(sql);
+		stmt.setString(1, categoria.getNm_categoria());
+		stmt.setString(2, categoria.getDs_categoria());
+
+		stmt.execute();
+		stmt.close();
+	}
 	
 	
 
--- src/repository/ServicoDAO.java
+++ src/repository/ServicoDAO.java
@@ -1,35 +1,72 @@
 package repository;
 
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import connectionFactory.ConnectionFactory;
 
 import model.Servico;
 
+
 public class ServicoDAO {
-	
-private Connection conexao;
-	
-	
+
+	private Connection conexao;
+
 	public ServicoDAO() {
 		this.conexao = new ConnectionFactory().conectar();
 	}
-	
+
 	public void insert(Servico servico) throws SQLException {
 		String sql = "insert into T_AUTO_SERVICO (cd_servico,nm_servico,ds_endereco) values (?,?,?)";
 		PreparedStatement stmt = conexao.prepareStatement(sql);
-		
+
 		stmt.setLong(1, 1L);
 		stmt.setString(2, "Seguro Auto");
 		stmt.setString(3, "Um otimo seguro para o seu carro");
-		
+
 		stmt.execute();
 		stmt.close();
-		
-		
-		
+
+	}
+
+	public List<Servico> select() throws SQLException {
+		List<Servico> usuarios = new ArrayList<Servico>();
+		String sql = "select * from t_auto_servico";
+		PreparedStatement stmt = conexao.prepareStatement(sql);
+		ResultSet rs = stmt.executeQuery();
+		while (rs.next()) {
+			Servico servico = new Servico();
+			servico.setCd_servico(rs.getLong("Codigo: "));
+			servico.setNm_servico(rs.getString("Nome do servico: "));
+			servico.setDs_servico(rs.getString("Descrição do serviço: "));
+
+		}
+		rs.close();
+		stmt.close();
+		return usuarios;
+	}
+
+	public void delete(Long Cd_servico) throws SQLException {
+		String sql = "delete from t_auto_servico where Cd_servico=?";
+		PreparedStatement stmt = conexao.prepareStatement(sql);
+		stmt.setLong(1, Cd_servico);
+		stmt.execute();
+		stmt.close();
+	}
+
+	public void update(Servico servico) throws SQLException {
+		String sql = "update t_auto_servico set Nm_servico=?, Ds_servico=?,where Cd_servico=?";
+		PreparedStatement stmt = conexao.prepareStatement(sql);
+		stmt.setString(1, servico.getNm_servico());
+		stmt.setString(2, servico.getDs_servico());
+
+		stmt.execute();
+		stmt.close();
 	}
 
 }
